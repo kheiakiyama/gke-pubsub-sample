a25a51718b4df89392f3b124ef9ae56c541b131c
kheiakiyama <kheiakiyama@gmail.com>
2018-10-14 21:51:08 +0900

    gcp pubsub publisher sample code

---
+++ scheduled-job/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+// Imports the Google Cloud client library
+const PubSub = require('@google-cloud/pubsub');
+
+// Your Google Cloud Platform project ID
+const topicName = 'sample';
+
+// Instantiates a client
+const pubsubClient = new PubSub();
+
+const data = JSON.stringify({ foo: 'bar' });
+const dataBuffer = Buffer.from(data);
+
+pubsubClient
+  .topic(topicName)
+  .publisher()
+  .publish(dataBuffer)
+  .then(messageId => {
+    console.log(`Message ${messageId} published.`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
